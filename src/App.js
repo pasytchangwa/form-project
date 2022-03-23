b5f417401e42c762826eae9902e077b96518cae5
sylvestre <pasytchangwa@gmail.com>
2022-03-23 16:02:21 +0200

    Add code to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import text from './data';
 
 function App() {
+  let data = text;
+  const [paragraph, setParagraph] = useState(0);
+  const [presented, setPresented] = useState([])
+
+  const handleChange = (e) => {
+    let x = e.target.value;
+    setParagraph(x);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let ammount = parseInt(paragraph);
+
+    if (paragraph <= 0) {
+      ammount = 1;
+    }
+    if (paragraph > data.length) {
+      ammount = data.length;
+    }
+
+    let newData = data.slice(0, ammount);
+    setPresented(newData)
+    console.log(newData);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='section-center'>
+        <h3>Tired of boring lorem ipsum?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='paragraphs'>paragraphs: </label>
+          <input
+            type='number'
+            name='paragraphs'
+            id='paragraphs'
+            value={paragraph}
+            onChange={handleChange}
+          />
+          <button className='btn' type='submit'>
+            Generate
+          </button>
+        </form>
+        <div className='lorem-text'>
+          {presented.map((present, index) => {
+            return <p key={index}>{present}</p>;
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
